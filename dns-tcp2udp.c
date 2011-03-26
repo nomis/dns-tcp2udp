abe045e128577c60eae5ac287d0cabe90f11eccc
Simon Arlott <simon.arlott.org.uk>
2011-03-26 17:57:33 +0000

    move code into separate functions

--- dns-tcp2udp.c
+++ dns-tcp2udp.c
@@ -32,19 +32,49 @@ static int out[MAXCONN];          // outgoing sockets (can only exist while an i
 static char resp[BUFSZ];          // response buffer
 
 static inline int max(int a, int b) { return a > b ? a : b; }
+static void setup(struct addrinfo **dest, int sockets, int server[], char *argv[]);
+static void drop_root(char *name);
+static void background(char *name);
+static void loop(int sockets, int server[], struct addrinfo *dest);
+static void accept_new(int sockets, int server[], fd_set *rfds, time_t now);
+static void incoming_read(int id, fd_set *efds, struct addrinfo *dest);
+static void forwarding_read(int id, fd_set *efds, time_t now);
+static void cleanup_inactive(time_t now);
+static void close_conn(int id);
 
 int main(int argc, char *argv[]) {
 	int sockets = max(0, argc - 2);     // listening socket count
 	int server[sockets];                // listening sockets
-	struct addrinfo hints;
 	struct addrinfo *dest;
-	struct addrinfo *res;
-	int i;
-	int ret;
-	pid_t pid;
-	uint16_t msgsz;
 
-	assert(sizeof(msgsz) == SIZELEN);
+	assert(sizeof(uint16_t) == SIZELEN);
+
+	if (sockets <= 0) {
+		printf("Usage: %s <dest ip> <listen ip> [listen ip]...\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	setup(&dest, sockets, server, argv);
+	drop_root(argv[0]);
+	background(argv[0]);
+	
+	// main loop
+	while (1) {
+		loop(sockets, server, dest);
+	}
+
+	freeaddrinfo(dest);
+	dest = NULL;
+
+	exit(EXIT_FAILURE);
+}
+
+static void setup(struct addrinfo **dest, int sockets, int server[], char *argv[]) {
+	struct addrinfo hints;
+	struct addrinfo *res;
+	int ret, i;
+
+	// initialise arrays
 	for (i = 0; i < MAXCONN; i++) {
 		in[i] = -1;
 		len[i] = 0;
@@ -53,11 +83,6 @@ int main(int argc, char *argv[]) {
 	}
 	used = 0;
 
-	if (sockets <= 0) {
-		printf("Usage: %s <dest ip> <listen ip> [listen ip]...\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
 	// prepare destination address
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
@@ -65,13 +90,13 @@ int main(int argc, char *argv[]) {
 	hints.ai_flags = AI_NUMERICHOST;
 	hints.ai_protocol = IPPROTO_UDP;
 
-	ret = getaddrinfo(argv[1], "domain", &hints, &dest);
+	ret = getaddrinfo(argv[1], "domain", &hints, dest);
 	if (ret != 0) {
 		fprintf(stderr, "%s: %s\n", argv[1], gai_strerror(ret));
 		exit(EXIT_FAILURE);
 	}
 
-	if (dest == NULL) {
+	if (*dest == NULL) {
 		fprintf(stderr, "Unable to parse destination host \"%s\"\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
@@ -131,235 +156,251 @@ int main(int argc, char *argv[]) {
 		freeaddrinfo(res);
 		res = NULL;
 	}
+}
 
-	// drop root privileges
+static void drop_root(char *name) {
+	int ret;
+
 	ret = setregid(GID, GID);
 	if (ret != 0) {
-			fprintf(stderr, "%s: unable to change GID: %s\n", argv[0], strerror(errno));
+			fprintf(stderr, "%s: unable to change GID: %s\n", name, strerror(errno));
 			exit(EXIT_FAILURE);
 	}
 	
 	ret = setreuid(UID, UID);
 	if (ret != 0) {
-			fprintf(stderr, "%s: unable to change UID: %s\n", argv[0], strerror(errno));
+			fprintf(stderr, "%s: unable to change UID: %s\n", name, strerror(errno));
 			exit(EXIT_FAILURE);
 	}
+}
 
-	// background
-	pid = fork();
+static void background(char *name) {
+	pid_t pid = fork();
 	if (pid > 0) {
-		printf("%s: started successfully with PID %d\n", argv[0], pid);
+		printf("%s: started successfully with PID %d\n", name, pid);
 		exit(EXIT_SUCCESS);
 	} else if (pid < 0) {
-		printf("%s: unable to fork: %s\n", argv[0], strerror(errno));
+		printf("%s: unable to fork: %s\n", name, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void loop(int sockets, int server[], struct addrinfo *dest) {
+	fd_set rfds, wfds, efds;
+	struct timeval timeout;
+	int maxfd = -1;
+	time_t now;
+	int ret, i;
+
+	// determine sockets to wait for
+	FD_ZERO(&rfds);
+	FD_ZERO(&wfds);
+	FD_ZERO(&efds);
+	timeout.tv_sec = CLEANUP_INTERVAL;
+	timeout.tv_usec = 0;
+
+	// listen for new connections if there are spare slots
+	if (used < MAXCONN) {
+		for (i = 0; i < sockets; i++) {
+			FD_SET(server[i], &rfds);
+			maxfd = max(maxfd, server[i]);
+		}
+	}
+
+	// wait for activity on connections
+	for (i = 0; i < MAXCONN; i++) {
+		if (in[i] != -1) {
+			FD_SET(in[i], &rfds);
+			FD_SET(in[i], &efds);
+			maxfd = max(maxfd, in[i]);
+		}
+
+		if (out[i] != -1) {
+			FD_SET(out[i], &rfds);
+			FD_SET(out[i], &efds);
+			maxfd = max(maxfd, out[i]);
+		}
+	}
+
+	// wait for activity (forever if no active client sockets)
+	ret = select(maxfd + 1, &rfds, &wfds, &efds, used > 0 ? &timeout : NULL);
+	if (ret < 0) {
+		fprintf(stderr, "select: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
-	// main loop
-	while (1) {
-		fd_set rfds, wfds, efds;
-		struct timeval timeout;
-		int maxfd = -1;
-		time_t now;
+	now = time(NULL);
 
-		// determine sockets to wait for
-		FD_ZERO(&rfds);
-		FD_ZERO(&wfds);
-		FD_ZERO(&efds);
-		timeout.tv_sec = CLEANUP_INTERVAL;
-		timeout.tv_usec = 0;
+	accept_new(sockets, server, &rfds, now);
 
-		// listen for new connections if there are spare slots
-		if (used < MAXCONN) {
-			for (i = 0; i < sockets; i++) {
-				FD_SET(server[i], &rfds);
-				maxfd = max(maxfd, server[i]);
-			}
+	for (i = 0; i < MAXCONN; i++) {
+		// read from incoming socket
+		if (in[i] != -1 && FD_ISSET(in[i], &rfds)) {
+			incoming_read(i, &efds, dest);
 		}
 
-		// wait for activity on connections
-		for (i = 0; i < MAXCONN; i++) {
-			if (in[i] != -1) {
-				FD_SET(in[i], &rfds);
-				FD_SET(in[i], &efds);
-				maxfd = max(maxfd, in[i]);
-			}
-
-			if (out[i] != -1) {
-				FD_SET(out[i], &rfds);
-				FD_SET(out[i], &efds);
-				maxfd = max(maxfd, out[i]);
-			}
+		// read from forwarding socket
+		if (out[i] != -1 && FD_ISSET(out[i], &rfds)) {
+			forwarding_read(i, &efds, now);
 		}
 
-		// wait for activity (forever if no active client sockets)
-		ret = select(maxfd + 1, &rfds, &wfds, &efds, used > 0 ? &timeout : NULL);
-		if (ret < 0) {
-			fprintf(stderr, "select: %s\n", strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-
-		now = time(NULL);
-
-		// accept new connections
-		for (i = 0; i < sockets; i++) {
-			if (FD_ISSET(server[i], &rfds)) {
-				ret = accept4(server[i], NULL, NULL, SOCK_NONBLOCK);
-				if (ret >= 0) {
-					int j;
-
-					// find spare slot (there must be one, we don't wait if full up)
-					for (j = 0; j < MAXCONN; j++) {
-						if (in[j] == -1) {
-							in[j] = ret;
-							out[j] = -1;
-							last[j] = now;
-							used++;
-							break;
-						}
-					}
-				}
-			}
-		}
-
-		for (i = 0; i < MAXCONN; i++) {
-			// read from incoming socket
-			if (in[i] != -1 && FD_ISSET(in[i], &rfds)) {
-				int space = BUFSZ - len[i];
-
-				ret = recv(in[i], &buf[i], space, 0);
-				if (ret <= 0) {
-					// failed read: add socket to error set
-					FD_SET(in[i], &efds);
-				} else {
-					len[i] += ret;
-				}
-
-				// size header in buffer?
-				if (!FD_ISSET(in[i], &efds) && len[i] >= SIZELEN) {
-					memcpy(&msgsz, buf[i], SIZELEN);
-					msgsz = ntohs(msgsz);
-
-					// complete message?
-					if (msgsz == 0) {
-						// zero length message: add socket to error set
-						FD_SET(in[i], &efds);
-					} else if (len[i] >= SIZELEN + msgsz) {
-						// open forwarding socket if it does not exist
-						if (out[i] == -1) {
-							ret = socket(dest->ai_family, dest->ai_socktype, dest->ai_protocol);
-							if (ret < 0) {
-								// failed open: add socket to error set
-								FD_SET(in[i], &efds);
-							} else {
-								int flags;
-
-								out[i] = ret;
-
-								flags = fcntl(out[i], F_GETFL);
-								flags |= O_NONBLOCK;
-								ret = fcntl(out[i], F_SETFL, flags);
-								if (ret != 0) {
-									// failed fcntl: add socket to error set
-									FD_SET(out[i], &efds);
-								} else {
-									ret = connect(out[i], dest->ai_addr, dest->ai_addrlen);
-									if (ret != 0) {
-										// failed connect: add socket to error set
-										FD_SET(out[i], &efds);
-									}
-								}
-							}
-						}
-
-						// check forwarding socket is ok
-						if (out[i] != -1 && !FD_ISSET(out[i], &efds)) {
-							// write data
-							ret = send(out[i], &buf[i][SIZELEN], msgsz, 0);
-							if (ret < 0) {
-								// failed write: add socket to error set
-								FD_SET(out[i], &efds);
-							}
-						}
-
-						// move remaining buffer data
-						len[i] -= SIZELEN;
-						len[i] -= msgsz;
-						if (len[i] > 0) {
-							memmove(buf[i], &buf[i][SIZELEN + msgsz], len[i]);
-						}
-					}
-				}
-			}
-
-			// read from forwarding socket
-			if (out[i] != -1 && FD_ISSET(out[i], &rfds)) {
-				int resp_len;
-
-				resp_len = recv(out[i], &resp[SIZELEN], MAXLEN, 0);
-				if (resp_len <= 0) {
-					// failed read: add socket to error set
-					FD_SET(out[i], &efds);
-				} else {
-					// prepend length
-					msgsz = htons(resp_len);
-					memcpy(resp, &msgsz, SIZELEN);
-
-					// send data
-					ret = send(in[i], &resp, SIZELEN + resp_len, 0);
-					if (ret < SIZELEN + resp_len) {
-						// failed write: add socket to error set
-						FD_SET(in[i], &efds);
-					} else {
-						// success: reset timeout
-						last[i] = now;
-					}
-				}
-			}
-
-			// handle errors on either socket by closing both sockets
-			if (in[i] != -1) {
-				if (FD_ISSET(in[i], &efds) || (out[i] != -1 && FD_ISSET(out[i], &efds))) {
-					close(in[i]);
-					in[i] = -1;
-					used--;
-
-					if (out[i] != -1) {
-						close(out[i]);
-						out[i] = -1;
-					}
-				}
-			}
-		}
-
-		// cleanup inactive sockets
-		//
-		// a socket is considered inactive if we haven't
-		// forwarded a reply to it - so this will include
-		// sockets that haven't sent a full request packet
-		// too
-		for (i = 0; i < MAXCONN; i++) {
-			if (in[i] != -1) {
-				if (now - last[i] >= CLEANUP_TIMEOUT) {
-					// close both sockets
-					close(in[i]);
-					in[i] = -1;
-					used--;
-
-					if (out[i] != -1) {
-						close(out[i]);
-						out[i] = -1;
-					}
-				} else if (now < last[i]) {
-					// oops, time went backwards
-					last[i] = now;
-				}
+		// handle errors on either socket by closing both sockets
+		if (in[i] != -1) {
+			if (FD_ISSET(in[i], &efds) || (out[i] != -1 && FD_ISSET(out[i], &efds))) {
+				close_conn(i);
 			}
 		}
 	}
 
-	freeaddrinfo(dest);
-	dest = NULL;
+	cleanup_inactive(now);
+}
 
-	exit(EXIT_FAILURE);
+static void accept_new(int sockets, int server[], fd_set *rfds, time_t now) {
+	int ret, i;
+
+	for (i = 0; i < sockets; i++) {
+		if (FD_ISSET(server[i], rfds)) {
+			ret = accept4(server[i], NULL, NULL, SOCK_NONBLOCK);
+			if (ret >= 0) {
+				int j;
+
+				// find spare slot (there must be one, we don't wait if full up)
+				for (j = 0; j < MAXCONN; j++) {
+					if (in[j] == -1) {
+						in[j] = ret;
+						out[j] = -1;
+						last[j] = now;
+						used++;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+static void incoming_read(int id, fd_set *efds, struct addrinfo *dest) {
+	int space = BUFSZ - len[id];
+	int ret;
+	uint16_t msgsz;
+
+	ret = recv(in[id], &buf[id], space, 0);
+	if (ret <= 0) {
+		// failed read: add socket to error set
+		FD_SET(in[id], efds);
+	} else {
+		len[id] += ret;
+	}
+
+	// size header in buffer?
+	if (!FD_ISSET(in[id], efds) && len[id] >= SIZELEN) {
+		memcpy(&msgsz, buf[id], SIZELEN);
+		msgsz = ntohs(msgsz);
+
+		// complete message?
+		if (msgsz == 0) {
+			// zero length message: add socket to error set
+			FD_SET(in[id], efds);
+		} else if (len[id] >= SIZELEN + msgsz) {
+			// open forwarding socket if it does not exist
+			if (out[id] == -1) {
+				ret = socket(dest->ai_family, dest->ai_socktype, dest->ai_protocol);
+				if (ret < 0) {
+					// failed open: add socket to error set
+					FD_SET(in[id], efds);
+				} else {
+					int flags;
+
+					out[id] = ret;
+
+					flags = fcntl(out[id], F_GETFL);
+					flags |= O_NONBLOCK;
+					ret = fcntl(out[id], F_SETFL, flags);
+					if (ret != 0) {
+						// failed fcntl: add socket to error set
+						FD_SET(out[id], efds);
+					} else {
+						ret = connect(out[id], dest->ai_addr, dest->ai_addrlen);
+						if (ret != 0) {
+							// failed connect: add socket to error set
+							FD_SET(out[id], efds);
+						}
+					}
+				}
+			}
+
+			// check forwarding socket is ok
+			if (out[id] != -1 && !FD_ISSET(out[id], efds)) {
+				// write data
+				ret = send(out[id], &buf[id][SIZELEN], msgsz, 0);
+				if (ret < 0) {
+					// failed write: add socket to error set
+					FD_SET(out[id], efds);
+				}
+			}
+
+			// move remaining buffer data
+			len[id] -= SIZELEN;
+			len[id] -= msgsz;
+			if (len[id] > 0) {
+				memmove(buf[id], &buf[id][SIZELEN + msgsz], len[id]);
+			}
+		}
+	}
+}
+
+static void forwarding_read(int id, fd_set *efds, time_t now) {
+	uint16_t msgsz;
+	int ret, resp_len;
+
+	resp_len = recv(out[id], &resp[SIZELEN], MAXLEN, 0);
+	if (resp_len <= 0) {
+		// failed read: add socket to error set
+		FD_SET(out[id], efds);
+	} else {
+		// prepend length
+		msgsz = htons(resp_len);
+		memcpy(resp, &msgsz, SIZELEN);
+
+		// send data
+		ret = send(in[id], &resp, SIZELEN + resp_len, 0);
+		if (ret < SIZELEN + resp_len) {
+			// failed write: add socket to error set
+			FD_SET(in[id], efds);
+		} else {
+			// success: reset timeout
+			last[id] = now;
+		}
+	}
+}
+
+static void cleanup_inactive(time_t now) {
+	int i;
+
+	// a socket is considered inactive if we haven't
+	// forwarded a reply to it - so this will include
+	// sockets that haven't sent a full request packet
+	// too
+	for (i = 0; i < MAXCONN; i++) {
+		if (in[i] != -1) {
+			if (now - last[i] >= CLEANUP_TIMEOUT) {
+				close_conn(i);
+			} else if (now < last[i]) {
+				// oops, time went backwards
+				last[i] = now;
+			}
+		}
+	}
+}
+
+static void close_conn(int id) {
+	// close both sockets
+	close(in[id]);
+	in[id] = -1;
+	used--;
+
+	if (out[id] != -1) {
+		close(out[id]);
+		out[id] = -1;
+	}
 }

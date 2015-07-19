af521bf93046576ff19f8de6d39b26e6c057a530
Simon Arlott <sa.me.uk>
2015-07-19 09:54:54 +0100

    return EXIT code instead of using exit() in the main() function

--- dns-tcp2udp.cpp
+++ dns-tcp2udp.cpp
@@ -12,19 +12,19 @@ int main(int argc, char *argv[]) {
 	list<string> args;
 
 	while (argc-- > 0)
-		args.push_back((argv++)[0]);
+		args.emplace_back((argv++)[0]);
 
 	string name = args.front();
 	args.pop_front();
 
 	if (args.size() < 2) {
 		cout << "Usage: " << name << " <dest ip> <listen ip> [listen ip]...\n";
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}
 
 	string dest = args.front();
 	args.pop_front();
 
 	Proxy(name, args, dest).mainLoop();
-	exit(EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
